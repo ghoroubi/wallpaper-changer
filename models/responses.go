9a7d6ef88150d6f599d4c749d02005fc12e84fcb
Nima Ghoroubi <ghoroubi85@gmail.com>
2020-12-22 15:21:58 +0330

    models definition

--- models/responses.go
+++ models/responses.go
@@ -3,5 +3,17 @@ package models
 // APIResponse
 // This models contains the response of the bing API.
 type APIResponse struct {
+	Images []*Image `json:"images"`
+}
 
+// Image ...
+type Image struct {
+	StartDate     string
+	FullStartDate string
+	EndDate       string
+	URL           string
+	URLBase       string
+	Copyright     string
+	CopyrightLink string
+	Title         string
 }
